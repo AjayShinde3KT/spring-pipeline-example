cea4581f25fef3bfc8ac2af5ee1aee1b408b5b11
Ajay Shinde <ajay.s@3ktechnologies.com>
2023-10-20 09:07:51 +0530

    first commit

---
+++ src/main/java/com/spring/pipeline/example/SpringPipelineExampleApplication.java
@@ -0,0 +1,13 @@
+package com.spring.pipeline.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPipelineExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPipelineExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/pipeline/example/SpringPipelineExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.pipeline.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPipelineExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
